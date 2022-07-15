f66c5191b3966629c5c2517341b7a20f849c5b94
ORAN <46417116+OranWeb@users.noreply.github.com>
2022-07-15 22:43:24 +0530

    Create TORN Forums Ban.user.js

---
+++ TORN Forums Ban.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         TORN Forums Ban
+// @version      1.0
+// @description  Illegal script to ban yourself from the torn city forums
+// @author       OranWeb
+// @match        https://www.torn.com/forums.php*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.location.href = "https://www.torn.com/index.php";
+})();
